9851c33f15d172448bb5d06f26d6fd667b60e8c4
MokshBirla <mokshitbirla@gmail.com>
2026-08-08 20:40:28 +0530

    Add function to shift zeros in an array
    Implemented a function to shift zeros to the end of the array and print the modified array.

--- Zeros.cpp
+++ Zeros.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-void ZerosAtLast
+void shiftzeros(int arr[], int n) {
+    for (int i = 0; i < n; i++) { //Check for the Zeros in array
+        if (arr[i] != 0) {
+            continue;
+        }
+        else {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] != 0) {    //swap zero with the nearest non zero element on the right side of array
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    break;
+                }
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
+    for (int i = 0; i < n; i++) {    //to print the array 
+        cout << arr[i] << " ";
+    }
+}
 
 int main() {
-    return 0;
+    int arr[] = {1, 2, 6, 0, 0, 43, 0, 5};
+    shiftzeros(arr, 8);
 }
